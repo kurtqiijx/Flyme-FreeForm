d29392ae7f5bbbebc90cf8e8e72cb2072a87ff92
kurtqiijx <kurtregidor2@gmail.com>
2026-09-07 12:49:58 +0800

    Android CI

--- app/src/main/java/com/sunshine/freeform/ui/freeform/FreeformView.kt
+++ app/src/main/java/com/sunshine/freeform/ui/freeform/FreeformView.kt
@@ -1047,12 +1047,7 @@ private fun addCloseButton() {
 private fun genCenterLocation(): IntArray {
     val center = intArrayOf(0, 0)
     if (!FreeformHelper.screenIsPortrait(screenRotation)) {
-        center[0] = (freeformWidth - rootHeight + screenPaddingX) / 2
-        if (!hangUpPosition[0])
-            center[0] = (freeformWidth - rootHeight + screenPaddingX) / -2
-        if (virtualDisplayRotation == VIRTUAL_DISPLAY_ROTATION_LANDSCAPE) {
-            center[0] = 0
-        }
+        center[0] = 0
     }
     return center
 }
